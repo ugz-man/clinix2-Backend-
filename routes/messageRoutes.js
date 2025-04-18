68be5d44865a19aed51d85047a245962f636d271
Ikedinma Ugochukwum <ugocodes@gmail.com>
2025-04-18 08:44:20 +0100

    modified the routes arrangement to correct the uploading image error

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -7,12 +7,13 @@ const router = express.Router();
 // expressMongoSanitize({ router });
 
 router.route("/").post(messageController.sendMessage);
-router.route("/:userId").post(messageController.deleteUserMessages);
 router
   .route("/image-file")
   .post(
     messageController.uploadImageFile,
     messageController.resizeAndSendImageFile
   );
+router.route("/:userId").post(messageController.deleteUserMessages);
+
 
 module.exports = router;
